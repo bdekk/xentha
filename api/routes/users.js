b882b177417fe7ae09c2c8bbd9c92df4dbb54d5b
Bram Dekker <bram.dekker.nl@gmail.com>
2016-07-29 21:22:46 +0200

    removed unnecessary comments

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,10 +4,8 @@ var user_controller = require('../controllers/users');
 
 router.get('/', function(req, res, next) {
 	return user_controller.get(req, res, next);
-//	res.send('Rooms')
 })
 
-// Car models page
 router.get('/:id', function(req, res, next) {
 	return user_controller.getOne(req, res, next);
 })
