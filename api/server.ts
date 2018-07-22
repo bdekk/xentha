9cbd82e844aa0096e0a8b0b149c6c41e06c35097
Bram Dekker <bram.dekker.nl@gmail.com>
2018-07-22 13:36:38 +0200

    fixed webpack start issue

--- api/server.ts
+++ api/server.ts
@@ -1,20 +1,24 @@
 var app = require('./src/app/app');
 var models = require("./src/app/models");
+
 //http server with sockets.
 //var ioserver = require('./ioserver');
 var server = require('http').Server(app);
 // var io = require('socket.io')(server, {'pingInterval': 45000});
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ server: server });
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({ server: server })
+
 // io.sockets.on('connection', function ( socket ) {
 //   console.log('client connected ', socket.id);
 // });
 var io = require('./src/app/ioserver')(wss);
+
 models.sequelize.sync().then(function () {
-    server.listen(app.get('port'), function () {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+  server.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+  });
 });
+
 // server.listen(app.get('ioport'),function() {
 // 	console.log('HTTPServer with websockets is listening on port ' + app.get('ioport'));
 // });
--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./server.js",
     output: {
-        filename: "./dist/bundle.js",
+        filename: "bundle.js",
     },
     mode: 'development',
     // Enable sourcemaps for debugging webpack's output.
